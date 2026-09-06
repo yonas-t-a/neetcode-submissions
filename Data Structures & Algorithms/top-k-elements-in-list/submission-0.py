2ba3ee04d68261f8e1083a9c974a1f0a9ed3de09
Yonas <155346881+yonas-t-a@users.noreply.github.com>
2026-09-06 20:11:18 +0300

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash = Counter(nums)
+
+        sorted_hash = dict(sorted(hash.items(), key=lambda item: item[1], reverse=True))
+
+        soln = []
+
+        for key, val in sorted_hash.items():
+            if k:
+                soln.append(key)
+                k-=1
+        
+        return soln
+            
+
+
+
